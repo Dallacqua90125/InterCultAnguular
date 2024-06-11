acfed63d03734c843cc3a845a07c9b5b2feee3d2
Txxxde <158209816+Txxxde@users.noreply.github.com>
2024-06-11 09:59:35 -0300

    bandeiras botoes

--- InterCult/src/app/app-routing.module.ts
+++ InterCult/src/app/app-routing.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ContentComponent } from './pages/content/content.component';
 import { HomeComponent } from './pages/home/home.component';
+import { EuaComponent } from './pages/eua/eua.component';
+import { FranceComponent } from './pages/france/france.component';
+import { UkComponent } from './pages/uk/uk.component';
+import { ItalyComponent } from './pages/italy/italy.component';
 
 const routes: Routes = [
   { path: 'sign-up', component: SignUpComponent },
   { path: '', component: LoginComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'content', component: ContentComponent },
+  { path: 'eua', component: EuaComponent },
+  { path: 'france', component: FranceComponent },
+  { path: 'uk', component: UkComponent },
+  { path: 'italy', component: ItalyComponent }
 ];
 
 @NgModule({
